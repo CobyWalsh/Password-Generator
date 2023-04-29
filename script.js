dc36ba35be961026f5169acfda47011d3ba379b0
coby walsh <cobywalsh7@yahoo.com>
2023-04-29 11:00:39 -0600

    added code to function writePassword to make the generate password button on the webpage work correctly

--- script.js
+++ script.js
@@ -3,20 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
   const password = Math.random().toString(36).slice(-8);
   const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
-//const generateBtn = document.querySelector('#generate-btn');
-generateBtn.addEventListener("click", writePassword); {
-  //const password = Math.random().toString(36).slice(-8);
-  //const passwordDisplay = document.querySelector('#password');
-  //passwordDisplay.textContent = password;
-}
+generateBtn.addEventListener("click", writePassword);
+  
 
